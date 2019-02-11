ff74e5dfd1065e38387908e6c1fb7b020c30f877
Marcos Poveda <marcospoveda@live.com>
2019-02-11 21:24:14 -0200

    Inicializando projeto curso Caelum WD-47

---
+++ agenda.js
@@ -0,0 +1,74 @@
+(function(){
+    console.log("start app...");
+
+    var endpoint = "http://localhost:4000/schedule";
+
+    // elementos da tela
+    var ui = {
+        fields: document.querySelectorAll("input"),
+        button: document.querySelector(".pure-button"),
+        table: document.querySelector(".pure-table")
+    };
+
+    // ações da tela
+    var validateFields = function(e){
+        // debugger;
+        e.preventDefault();
+        console.log("botão clicado", ui.fields);
+        var data = {};
+        var errors = 0;
+        ui.fields.forEach(function(field){
+            var fieldValue = field.value.trim();
+            // console.log(field.value, field.value.length, field.id);
+            if(fieldValue.length === 0){
+                field.classList.add("error");
+                errors++;
+            } else {
+                field.classList.remove("error");
+                data[field.id] = fieldValue;
+            }
+        });
+
+        // // Arrow Function
+        // ui.fields.forEach(field => {
+        //     if(field.value.trim().length === 0){
+        //         field.classList.add("error");
+        //         errors++;
+        //     } else {
+        //         field.classList.remove("error");
+        //         data[field.id] = field.value.trim();
+        //     }
+        // });
+
+        if(errors > 0){
+            document.querySelector(".error").focus();
+        } else {
+            addContact(data);
+        }
+    };
+
+    var addContact = function(contact){
+        console.log(contact);
+        //forma de envio da requisição HTTP (GET, POST, PUT, DELETE) - method
+        //Corpo da requisição (Envia os dados), 'stringify' converte o objeto para JSON - body
+        //Definição do formato do arquivo enviado (json, binário, authentication......) - headers
+        var config = {
+            method: "post",
+            body: JSON.stringify(contact),
+            headers: new Headers({
+                "Content-Type": "application/json"
+            })
+        };
+        console.log(config);
+    };
+
+    var getContacts = function(){};
+
+    var removeContact = function(){};
+
+    var init = function(){
+        ui.button.onclick = validateFields;
+    }();
+
+    console.log(ui);
+})();
